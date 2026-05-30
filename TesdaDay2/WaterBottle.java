7680b3b23e0770f33f88c59042e9491da8f69da5
pkharyllmorales <p.kharyllmorales@gmail.com>
2026-05-30 20:17:48 +0800

    Chap2 Activity

--- TesdaDay2/WaterBottle.java
+++ TesdaDay2/WaterBottle.java
@@ -1,12 +1,33 @@
-package WaterBottle;
 
- public class WaterBottle {
-private String brand;
- private boolean empty;
- public static void main(String[] args) {
-        WaterBottle wb = new WaterBottle();
-        System.out.print("Empty = " + wb.empty);
-     System.out.print(", Brand = " + wb.brand);
-} }
 
- 
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for input
+        System.out.print("Enter three numbers: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        // Check if all numbers are equal first
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("All numbers are equal");
+        } else {
+            // Assume the first number is the largest initially
+            int largest = num1;
+
+            if (num2 > largest) {
+                largest = num2;
+            }
+            if (num3 > largest) {
+                largest = num3;
+            }
+
+            System.out.println("The largest number is " + largest);
+        }
+
+        scanner.close();
+    }
+}
