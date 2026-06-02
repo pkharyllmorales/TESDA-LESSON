14f3643f4686278f41b3adcc490b4a19ee36b20a
pkharyllmorales <p.kharyllmorales@gmail.com>
2026-06-02 15:52:18 +0800

    Task 2

--- LargestNumber_Activity/LargestNumber.java
+++ LargestNumber_Activity/LargestNumber.java
@@ -6,28 +6,33 @@ import java.util.Scanner;
 public class LargestNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.print("Enter three numbers: ");
+            int num1 = scanner.nextInt();
+            int num2 = scanner.nextInt();
+            int num3 = scanner.nextInt();
 
-        // Prompt the user for input
-        System.out.print("Enter three numbers: ");
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        int num3 = scanner.nextInt();
+            if (num1 == num2 && num2 == num3) {
+                System.out.println("All numbers are equal!");
+            } else {
+                int largest = num1; //num1
+                if (num2 > largest) {
+                    largest = num2; //num2
+                }
+                if (num3 > largest) {
+                    largest = num3; //num3
+                }
 
-        // Check if all numbers are equal first
-        if (num1 == num2 && num2 == num3) {
-            System.out.println("All numbers are equal");
-        } else {
-            // Assume the first number is the largest initially
-            int largest = num1;
-
-            if (num2 > largest) {
-                largest = num2;
+                System.out.println("The largest number isssss " + largest + "!!");
             }
-            if (num3 > largest) {
-                largest = num3;
+            
+            System.out.print("Do you want to enter more numbers? (yes/no): ");
+            String response = scanner.next();
+            if (!response.equalsIgnoreCase("yes")) {
+                System.out.println("Goodbye!");
+                break;
             }
-
-            System.out.println("The largest number is " + largest);
         }
 
         scanner.close();
