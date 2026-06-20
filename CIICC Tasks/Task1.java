5e42aa42085443dd7d9ff1b637f7648b8b537b33
pkharyllmorales <p.kharyllmorales@gmail.com>
2026-06-20 15:12:54 +0800

    Updated Task 1

--- CIICC Tasks/Task1.java
+++ CIICC Tasks/Task1.java
@@ -5,18 +5,3 @@ public class Task1 {
     }
 }
     
-
-
-4.What is the result of the following code?
-
-7: StringBuilder sb = new StringBuilder();
-8: sb.append("aaa").insert(1, "bb").insert(4, "ccc");
-9: System.out.println(sb);
-*
-1 point
-abbaaccc
-abbaccca
-bbaaaccc
-bbaaccca
-An exception is thrown.
-The code does not compile.
